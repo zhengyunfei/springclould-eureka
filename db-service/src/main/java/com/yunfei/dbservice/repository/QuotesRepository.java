664146a0663852f52d681b56a7d4c7650bfc947c
令狐冲169 <799078779@qq.com>
2019-01-14 17:30:09 +0800

    springclould-eureka

---
+++ db-service/src/main/java/com/yunfei/dbservice/repository/QuotesRepository.java
@@ -0,0 +1,10 @@
+package com.yunfei.dbservice.repository;
+
+import com.yunfei.dbservice.model.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+    List<Quote> findByUserName(String username);
+}
---
+++ db-service/src/main/java/com/yunfei/dbservice/resource/DbServiceResource.java
@@ -0,0 +1,43 @@
+package com.yunfei.dbservice.resource;
+
+import com.yunfei.dbservice.model.Quote;
+import com.yunfei.dbservice.model.Quotes;
+import com.yunfei.dbservice.repository.QuotesRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.stream.Collectors;
+@RestController
+@RequestMapping("/rest/db")
+public class DbServiceResource {
+    private QuotesRepository quotesRepository;
+    public DbServiceResource(QuotesRepository quotesRepository) {
+        this.quotesRepository = quotesRepository;
+    }
+    @GetMapping("/{username}")
+    public List<String> getQuotes(@PathVariable("username") final String username) {
+        return getQuotesByUserName(username);
+    }
+    @PostMapping("/add")
+    public List<String> add(@RequestBody final Quotes quotes) {
+        quotes.getQuotes()
+                .stream()
+                .map(quote -> new Quote(quotes.getUserName(), quote))
+                .forEach(quote -> quotesRepository.save(quote));
+        return getQuotesByUserName(quotes.getUserName());
+    }
+    @PostMapping("/delete/{username}")
+    public List<String> delete(@PathVariable("username") final String username) {
+        List<Quote> quotes = quotesRepository.findByUserName(username);
+        quotesRepository.delete(quotes);
+        return getQuotesByUserName(username);
+    }
+    private List<String> getQuotesByUserName(@PathVariable("username") String username) {
+        return quotesRepository.findByUserName(username)
+                .stream()
+                .map(Quote::getQuote)
+                .collect(Collectors.toList());
+    }
+
+
+
+}
